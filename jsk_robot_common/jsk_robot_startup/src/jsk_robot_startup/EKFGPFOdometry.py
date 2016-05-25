d1246b2b2fea8da491b6ed61b7b26f161663cfa6
orikuma <ik0313@gmail.com>
2016-05-25 20:31:56 +0900

    [jsk_robot_startup] Execute sampling and weighting in the same loop for calculation cost

--- jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/EKFGPFOdometry.py
+++ jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/EKFGPFOdometry.py
@@ -42,10 +42,8 @@ class EKFGPFOdometry(ParticleOdometry):
     def calc_odometry(self):
         self.ekf_update(self.odom, self.source_odom)
         if self.measurement_updated:
-            # sampling
-            self.particles = self.sampling(self.odom.pose)
-            # weighting
-            self.weights = self.weighting(self.particles, self.min_weight)
+            # sampling and weighting
+            self.particles, self.weights = self.sampling_and_weighting(self.odom.pose, self.min_weight)
             # resampling
             self.particles = self.resampling(self.particles, self.weights)
             # estimate new pdf 
@@ -61,11 +59,37 @@ class EKFGPFOdometry(ParticleOdometry):
         self.odom.pose = new_pose_with_covariance
                     
     ## particle filter functions
-    # sampling poses from EKF result (current_pose_with_covariance)
-    def sampling(self, current_pose_with_covariance):
-        pose_mean = self.convert_pose_to_list(current_pose_with_covariance.pose)
+    # input: EKF result (current_pose_with_covariance), min_weight(float) output: list of particles and weights
+    # sampling and weighting at the same time to reduce computational cost in loop calculation
+    def sampling_and_weighting(self, current_pose_with_covariance, min_weight):
+        pose_mean = self.convert_pose_to_list(current_pose_with_covariance.pose) 
         pose_cov_matrix = zip(*[iter(current_pose_with_covariance.covariance)]*6)
-        return [self.convert_list_to_pose(x) for x in numpy.random.multivariate_normal(pose_mean, pose_cov_matrix, int(self.particle_num)).tolist()]
+        weights = []
+        particles = []
+        if not self.measure_odom:
+            rospy.logwarn("[%s] measurement does not exist.", rospy.get_name())
+            particles = [self.convert_list_to_pose(x) for x in numpy.random.multivariate_normal(pose_mean, pose_cov_matrix, int(self.particle_num)).tolist()] # random sampling from ekf result distribution
+            weights = [1.0 / self.particle_num] * int(self.particle_num) # use uniform weights when measure_odom has not been subscribed yet
+        else:
+            measure_to_source_dt = (self.source_odom.header.stamp - self.measure_odom.header.stamp).to_sec() # adjust timestamp of pose in measure_odom to source_odom
+            current_measure_pose_with_covariance = self.update_pose_with_covariance(self.measure_odom.pose, self.measure_odom.twist, measure_to_source_dt) # assuming dt is small and measure_odom.twist is do not change in dt
+            measurement_pose_array = numpy.array(self.convert_pose_to_list(current_measure_pose_with_covariance.pose))
+            try:
+                measurement_cov_matrix_inv = numpy.linalg.inv(numpy.matrix(current_measure_pose_with_covariance.covariance).reshape(6, 6)) # calculate inverse matrix first to reduce computation cost
+                for i in range(int(self.particle_num)):
+                    prt = self.convert_list_to_pose(numpy.random.multivariate_normal(pose_mean, pose_cov_matrix))
+                    w = max(min_weight, self.calculate_weighting_likelihood(prt, measurement_pose_array, measurement_cov_matrix_inv))
+                    particles.append(prt)
+                    weights.append(w)
+            except numpy.linalg.LinAlgError:
+                rospy.logwarn("[%s] covariance matrix is not singular.", rospy.get_name())
+                particles = [self.convert_list_to_pose(x) for x in numpy.random.multivariate_normal(pose_mean, pose_cov_matrix, int(self.particle_num)).tolist()] # random sampling from ekf result distribution
+                weights = [min_weight] * len(particles)
+            if all([x == min_weight for x in weights]):
+                rospy.logwarn("[%s] likelihood is too small and all weights are limited by min_weight.", rospy.get_name())
+            normalization_coeffs = sum(weights) # normalization and each weight is assumed to be larger than 0
+            weights = [w / normalization_coeffs for w in weights]            
+        return particles, weights
 
     def approximate_odometry(self, particles, weights):
         # use only important particels
